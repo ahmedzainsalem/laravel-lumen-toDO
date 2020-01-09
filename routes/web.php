00d3a120f58611abff23ab5fa65ef808a5b2bd02
zain <ahmedzainsalem@gmail.com>
2020-01-09 12:38:54 +0400

    API (user profile, all users, get user by id)

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,13 @@ $router->group(['prefix' => 'api'], function () use ($router) {
       $router->post('register', 'RegisterController@register');
       // Matches "/api/login
       $router->post('login', 'RegisterController@login');
+       // Matches "/api/profile
+      $router->get('profile', 'UserController@profile');
+
+      // Matches "/api/users/1 
+      //get one user by id
+      $router->get('users/{id}', 'UserController@singleUser');
+
+      // Matches "/api/users
+      $router->get('users', 'UserController@allUsers');
  });
